8cda04642e629442f0f28e4489004ad096e320e2
Tim Eberle <58324439+timebr@users.noreply.github.com>
2019-11-29 14:11:44 +0100

    Reducing inconsitent code

--- PowerShellGui/SynchronisePolicies.xaml.cs
+++ PowerShellGui/SynchronisePolicies.xaml.cs
@@ -18,27 +18,5 @@ namespace PowerShellGui
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
-        {
-           
-        }
-
-        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
-        {
-            
-            
-        }
-
-        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-    }
+    
 }
