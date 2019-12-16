18be1ef62f1fbda2de8faf72701fe33706978268
David Buerge <budabrine@gmail.com>
2019-12-16 16:32:34 +0100

    First try for search

--- PowerShellGui/RegistryExport.xaml.cs
+++ PowerShellGui/RegistryExport.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace PowerShellGui
 {
@@ -105,6 +106,70 @@ namespace PowerShellGui
 
         private void ButtonSearch_Click(object sender, RoutedEventArgs e)
         {
+            TextRange textRange = new TextRange(RegistryExportTextBox.Document.ContentStart, RegistryExportTextBox.Document.ContentEnd);
+
+            //Clear up all highligted Text
+            textRange.ClearAllProperties();
+
+            //get the richtextbox text
+            string textBoxText = textRange.Text;
+
+            //get search text
+            string searchText = SearchTextBox.Text;
+
+            if (string.IsNullOrWhiteSpace(textBoxText) || string.IsNullOrWhiteSpace(searchText))
+            {
+                //Error Message
+            }
+            else
+            {
+                Regex regex = new Regex(searchText);
+                int count_MatchFound = Regex.Matches(textBoxText, regex.ToString()).Count;
+
+                for (TextPointer startPointer = RegistryExportTextBox.Document.ContentStart;
+                        startPointer.CompareTo(RegistryExportTextBox.Document.ContentEnd) <= 0;
+                        startPointer = startPointer.GetNextContextPosition(LogicalDirection.Forward))
+                {
+                    //check if end of text
+                    if (startPointer.CompareTo(RegistryExportTextBox.Document.ContentEnd) == 0)
+                    {
+                        break;
+                    }
+                    string parsedString = startPointer.GetTextInRun(LogicalDirection.Forward);
+
+                    //check if the search string present here
+                    int indexOfParseString = parsedString.IndexOf(searchText);
+
+                    if (indexOfParseString >= 0) //present
+                    {
+                        //setting up the pointer here at this matched index
+                        startPointer.GetPositionAtOffset(indexOfParseString);
+
+                        if (startPointer != null)
+                        {
+                            //next pointer will be the length of the search string
+                            TextPointer nextPointer = startPointer.GetPositionAtOffset(searchText.Length);
+
+                            //create the text range
+                            TextRange searchedTextRange = new TextRange(startPointer, nextPointer);
+
+                            //color up
+                            searchedTextRange.ApplyPropertyValue(TextElement.BackgroundProperty, new SolidColorBrush(Colors.Yellow));
+
+                            //Other settings for format
+                        }
+                    }
+                }
+                //update the label text with count
+                if (count_MatchFound > 0)
+                {
+                    //Report the number of matches
+                }
+                else
+                {
+                    //Report no match found
+                }
+            }
 
         }
 
